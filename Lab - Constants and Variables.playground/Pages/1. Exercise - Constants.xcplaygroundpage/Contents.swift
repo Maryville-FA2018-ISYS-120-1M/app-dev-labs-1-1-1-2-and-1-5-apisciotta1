b4930b097139da840e160a0034e98dcda1bf3e18
Anthony Pisciotta <apisciotta1@live.maryville.edu>
2018-09-02 21:37:33 -0500

    lab 1.2

--- Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
+++ Lab - Constants and Variables.playground/Pages/1. Exercise - Constants.xcplaygroundpage/Contents.swift
@@ -2,11 +2,11 @@
 /*:
  Now assume you go through and remove friends that aren't active on social media. Update your `friends` constant to a lower number than it currently is.
  */
-
-
+let friends = 341
+print(friends)
 /*:
  Does the above code compile? Why not? Print your explanation to the console using the `print` function. Go back and delete your line of code that updates the `friend` constant to a lower number so that the playground will compile properly.
  */
-
-
+print("No")
+print("It does not compile because the number of my friends is not a constant and changes after I have removed inactive friends.")
 //: page 1 of 10  |  [Next: App Exercise - Step Goal](@next)
